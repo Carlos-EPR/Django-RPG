dde1b0a5131e42bf3e705acd52bc8c73d12ed527
Carlos Eduardo <carlos_eduardopr123@hotmail.com>
2024-01-22 18:18:06 -0300

    10

--- rpg/settings.py
+++ rpg/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "templates/static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "templates\static")
 
 
 # Default primary key field type
